8e88ea439576890fd2d443e85a627db579902cc3
Likhin Nelliyotan <likhin@alisonsgroup.com>
2021-06-01 16:39:12 +0530

    english complete

--- lib/languages_en.dart
+++ lib/languages_en.dart
@@ -255,4 +255,158 @@ class LanguageEn extends Languages {
     'ordered by mistake',
     'find new model'
   ];
+  @override
+  String get Return => "Return";
+  @override
+  String get addNewAddress => "Add new Address";
+  @override
+  String get addToCart => "Add to Cart";
+  @override
+  String get addToWishList => "Add to Wishlist";
+  @override
+  String get address => "Address";
+  @override
+  String get addressCaps => "ADDRESS";
+  @override
+  String get apply => "Apply";
+  @override
+  String get applyFilters => "APPLY FILTERS";
+  @override
+  String get availability => "Availability";
+  @override
+  String get cart => "Cart";
+  @override
+  String get cashOnDelivery => "Cash on Delivery";
+  @override
+  String get checkout => "Checkout";
+  @override
+  String get clearAll => "Clear All";
+  @override
+  String get color => "Color";
+  @override
+  String get continueShopping => "Continue Shopping";
+  @override
+  String get currentBalance => "Current Balance";
+  @override
+  String get currentPassword => "Current Password";
+  @override
+  String get date => "Date";
+  @override
+  String get dateCaps => "DATE";
+  @override
+  String get delete => "Delete";
+  @override
+  String get delivery => "Delivery";
+  @override
+  String get edit => "Edit";
+  @override
+  String get emptyAddress => "Your Address list is Empty";
+  @override
+  String get emptyCart => "Your Cart is Empty";
+  @override
+  String get emptyNotification => "Your Notification is Empty";
+  @override
+  String get emptyProduct => "Your Product List is Empty";
+  @override
+  String get emptyStoreList => "Your Store list is Empty";
+  @override
+  String get emptyWishList => "Your Wish list is Empty";
+  @override
+  String get enterCurrentPassword => "Enter your current password";
+  @override
+  String get enterNewPassword => "Enter new password";
+  @override
+  String get history => "History";
+  @override
+  String get howItWorks => "How it works";
+  @override
+  String get inStock => "In Stock";
+  @override
+  String get moveToBag => "Move to bag";
+  @override
+  String get newPassword => "New password";
+  @override
+  String get noData => "No data";
+  @override
+  String get orderCancelRequested => "Order Cancel Requested";
+  @override
+  String get orderCancellation => "Order cancellation";
+  @override
+  String get passwordNotMatch => "Password do not match";
+  @override
+  String get payment => "Payment";
+  @override
+  String get paymentOptions => "Payment Options";
+  @override
+  String get personalInformation => "Personal Information";
+  @override
+  String get placeOrder => "Place Order";
+  @override
+  String get plsWriteReviewHere => "Please Write your review here";
+  @override
+  String get popularity => "Popularity";
+  @override
+  String get priceHighToLow => "Price - High to Low";
+  @override
+  String get priceLowToHigh => "Price - Low to High";
+  @override
+  String get productDescription => "Product Description";
+  @override
+  String get qty => "QTY :";
+  @override
+  String get rating => "Rating";
+  @override
+  String get reEnterPaassword => "Re - Enter Password";
+  @override
+  String get refer => "Refer";
+  @override
+  String get remove => "Remove";
+  @override
+  String get resetPassword => "Reset Password";
+  @override
+  String get reviews => "Reviews";
+  @override
+  String get selectPriceRange => "Select Price Range";
+  @override
+  String get selectReason => "Select Reason for cancellation";
+  @override
+  String get sizeChart => "Size Chart";
+  @override
+  String get sortBy => "Sort by";
+  @override
+  String get sortByCaps => "SORT BY";
+  @override
+  String get state => "State/Provinces";
+  @override
+  String get stateCaps => "STATE/PROVINCES";
+  @override
+  String get status => "Status";
+  @override
+  String get submit =>"Submit";
+  @override
+  String get tellUsMore => "Tell us more(optional_";
+  @override
+  String get time => "Time";
+  @override
+  String get timeCaps => "TIME";
+  @override
+  String get tracking =>"Tracking";
+  @override
+  String get trackingNo => "Tracking Number";
+  @override
+  String get upComing => "Up coming";
+  @override
+  String get viewDetails => "View details";
+  @override
+  String get writeReview => "Write Review";
+  @override
+  String get writeYourReviewHereCaps => "WRITE REVIEW";
+  @override
+  String get yourCart => "Your Cart";
+  @override
+  String get yourOrderNumberIs => "Your Order Number is";
+  @override
+  String get zipCode => "Zip/Postal Code";
+  @override
+  String get zipCodeCaps => "ZIP/POSTAL CODE";
 }
